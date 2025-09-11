7a678156bed702adae2722f8455c9507924d3050
hyuga127 <tranhuyzaza@gmail.com>
2025-09-11 17:34:04 +0700

    next

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,11 +18,12 @@ const userSchema = new mongoose.Schema(
     },
     isAdmin: {
       type: Boolean,
-      required: true,
+      required: false,
       default: false,
     },
     avatar: {
       type: String,
+      required: false,
       default:
         "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y",
     },
@@ -30,6 +32,26 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Kiểm tra và mã hóa mật khẩu trước khi lưu
+
+userSchema.pre("save", async function (next) {
+  // Chỉ mã hóa nếu mật khẩu được thay đổi hoặc mới tạo
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // Mã hóa mật khẩu với bcrypt và salt
+  // Salt là một chuỗi ngẫu nhiên được thêm vào mật khẩu trước khi băm để tăng cường bảo mật
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  // next() được gọi để tiếp tục quá trình lưu trữ
+  next();
+});
+
+// Phương thức để so sánh mật khẩu đã nhập với mật khẩu đã mã hóa trong cơ sở dữ liệu
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoute.js";
 
 dotenv.config();
-console.log("Environment variables loaded:");
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URI:", process.env.MONGO_URI);
-console.log("JWT_SECRET:", process.env.JWT_SECRET);
 connectDB();
 
 const app = express();
@@ -14,6 +11,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
